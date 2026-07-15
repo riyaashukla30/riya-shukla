f2da88ae10cfbc1e1fb23bbb59d3966c414c3b0c
Riya Shukla <riyaashukla30@gmail.com>
2026-07-15 14:07:00 +0530

    Update works.tsx

--- src/components/works.tsx
+++ src/components/works.tsx
@@ -1,4 +1,4 @@
-import { Tilt } from "react-tilt";
+import Tilt from "react-parallax-tilt";
 import { motion } from "framer-motion";
 
 import { github, preview } from "../assets";
@@ -24,11 +24,10 @@ const ProjectCard = ({
 }: ProjectCardProps) => (
   <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
     <Tilt
-      options={{
-        max: 45,
-        scale: 1,
-        speed: 450,
-      }}
+      tiltMaxAngleX={45}
+      tiltMaxAngleY={45}
+      scale={1}
+      transitionSpeed={450}
       className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
     >
       <div className="relative w-full h-[230px]">
@@ -42,28 +41,24 @@ const ProjectCard = ({
         {/* Live Site */}
         <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
           <div
-            onClick={() => window.open(live_site_link, "_blank", "noreferrer")}
+            onClick={() => window.open(live_site_link, "_blank")}
             className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
           >
             <img
               src={preview}
               alt="Live Site"
-              title="Live Site"
               className="w-2/3 h-2/3 object-contain"
             />
           </div>
 
           {/* Github */}
           <div
-            onClick={() =>
-              window.open(source_code_link, "_blank", "noreferrer")
-            }
+            onClick={() => window.open(source_code_link, "_blank")}
             className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer ml-2"
           >
             <img
               src={github}
               alt="Github"
-              title="Github"
               className="w-1/2 h-1/2 object-contain"
             />
           </div>
@@ -79,7 +74,7 @@ const ProjectCard = ({
       {/* Work Tag */}
       <div className="mt-4 flex flex-wrap gap-2">
         {tags.map((tag, tagIdx) => (
-          <p key={`Tag-${tagIdx}`} className={cn(tag.color, "text-[14px]")}>
+          <p key={`tag-${tagIdx}`} className={cn(tag.color, "text-[14px]")}>
             #{tag.name}
           </p>
         ))}
@@ -88,7 +83,7 @@ const ProjectCard = ({
   </motion.div>
 );
 
-// Works
+// Works Section
 export const Works = () => {
   return (
     <SectionWrapper>
@@ -99,21 +94,19 @@ export const Works = () => {
           <h2 className={styles.sectionHeadText}>Projects.</h2>
         </motion.div>
 
-        {/* About */}
+        {/* Description */}
         <div className="w-full flex">
           <motion.p
             variants={fadeIn("", "", 0.1, 1)}
             className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
           >
-            Following projects showcases my skills and experience through
-            real-world examples of my work. Each project is briefly described
-            with links to code repositories and live demos in it. It reflects my
-            ability to solve complex problems, work with different technologies,
-            and manage projects effectively.
+            Following projects showcase my skills and experience through
+            real-world examples. Each project includes links to source code and
+            live demos.
           </motion.p>
         </div>
 
-        {/* Project Card */}
+        {/* Project Cards */}
         <div className="mt-20 flex flex-wrap gap-7">
           {PROJECTS.map((project, i) => (
             <ProjectCard key={`project-${i}`} index={i} {...project} />
